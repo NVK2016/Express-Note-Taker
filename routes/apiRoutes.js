2710a841727fb280ef07d50f7c319194c5c02416
Namita Shenai <namita.shenai@gmail.com>
2020-03-14 00:13:59 -0700

    Add Note i nJSON file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,6 +26,36 @@ module.exports = function (app) {
 
     });
 
+    // API POST Request
+    app.post('/api/notes', function (req, res) {
+        console.log("Add new note");
+        //Read the JSON file 
+        fs.readFile("./db/db.json", "utf8", (err, response) => {
+            //convert the response to JSON 
+            let allNotes = JSON.parse(response);
+            //GRAB ID OF THE LAST ELEMENT FROM THE JSON FILE 
+            var lastID = allNotes[allNotes.length - 1].id;
+            //Increment to generate a new ID 
+            lastID = lastID + 1;
+            //Append the new id to the user created note  
+            const newNote = { ...req.body, id: lastID };
+
+            console.log(newNote);
+
+            allNotes = [...allNotes, newNote];
+
+            fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
+
+                if (err) throw res.status(500).json(err);
+
+                res.json({ success: true, msg: 'Created new note' });
+
+                console.log("Note created!", allNotes);
+
+            });
+
+        });
+    });
 
     // DELETE "/api/notes" deletes the note with an id equal to req.params.id
     app.delete('/api/notes/:id', (req, res) => {
